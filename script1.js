1e9dc73d303f82d77e8959f90eef4fdcc41c5def
jaajarbinks <mr.larsen@windowslive.com>
2022-07-11 23:17:38 -0600

    updates

--- script1.js
+++ script1.js
@@ -2,9 +2,6 @@ var APIKey = "0ae8e28782574a4e1da5362bb95aa91d";
 var api = "https://api.openweathermap.org/data/2.5/onecall?lat={39.613319}&lon={-105.016647}&appid={0ae8e28782574a4e1da5362bb95aa91d";
 
 
-
-
-
 const header = document.createElement('header');
 header.innerText = "WhyWeather";
 document.body.append(header);
@@ -18,3 +15,6 @@ setInterval(function () {
     var timeDate = moment().format("MMM Do, YYYY" + " hh:mm:ss");
     p.innerText = "Today is " + timeDate;
   }, 1000);
+
+  const main = document.createElement('main');
+  
